1b1de1a9927475a003e9ce12dc246ad4f8ebc2d2
helloai <282602809@qq.com>
2025-03-09 09:19:41 +0800

    ImageNet

--- ML/CNN(卷积神经网络)/cnn.py
+++ ML/CNN(卷积神经网络)/cnn.py
@@ -22,13 +22,28 @@ def extract_features(img_path, model):
     return features.flatten()
 
 # 图像路径
-image1_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/houge.jpg'
-image2_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/zhipiao.jpg'
+image1_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/beizi1.jpg'
+image2_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/maomao1.jpeg'
+image3_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/maomao2.jpeg'
+image4_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/test1.jpeg'
+image5_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/zhipiao.jpg'
+image6_path = '/Users/hang.yang/AI/pythonsamples-main/ML/CNN(卷积神经网络)/ImageVector/lakuten.jpg'
 
 # 提取特征向量
 features1 = extract_features(image1_path, model)
 features2 = extract_features(image2_path, model)
-
+features3 = extract_features(image3_path, model)
+features4 = extract_features(image4_path, model)
+features5 = extract_features(image5_path, model)
+features6 = extract_features(image6_path, model)
 # 计算余弦相似度
 similarity = cosine_similarity([features1], [features2])[0][0]
-print("相似度:", similarity)
+print("杯子和猫 相似度:", similarity)
+similarity = cosine_similarity([features2], [features3])[0][0]
+print("两只猫 相似度:", similarity)
+similarity = cosine_similarity([features4], [features5])[0][0]
+print("支票和Logo1 相似度:", similarity)
+similarity = cosine_similarity([features5], [features6])[0][0]
+print("支票和Logo2 相似度:", similarity)
+similarity = cosine_similarity([features4], [features6])[0][0]
+print("Logo1和Logo2 相似度:", similarity)
